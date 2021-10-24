7142fd8efd421269c17b433b9ffa22497dbf9096
Ryan <49902829+ryantanrk@users.noreply.github.com>
2021-10-24 19:06:05 +0800

    post method working

--- api/create/createperson.php
+++ api/create/createperson.php
@@ -1,6 +1,43 @@
 <?php
-    //get received input and returns an associative array
-    $body = json_decode(file_get_contents('php://input'), true);
+    require_once '../../connection.php';
+    //get received input
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        //get attributes
+        $personID = $_POST['personID'];
+        $type = $_POST['type'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $email = $_POST['email'];
+        $dob = $_POST['dob'];
 
-    echo json_encode($body, JSON_PRETTY_PRINT);
+        //insert query
+        $query = "INSERT INTO `$personTable` VALUES ($personID, $type, $username, $password, $email, $dob)";
+
+        //if query is successful, echo success message, if not echo error
+        if ($connection->query($query) === TRUE) {
+            echo "New record created successfully";
+        }
+        else {
+            echo "Error: " . $query . "<br>" . $connection->error;
+        }
+
+        $connection->close(); //close connection
+    }
+    else {
+        echo "No object received";
+    }
+
+    // require_once '../../connection.php';
+
+    // if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //     $text = $_POST['text'];
+    //     $query = "INSERT INTO react_php (`id`, `text`)  VALUES (NULL, '$text')";
+
+    //     if ($connection->query($query) === TRUE) {
+    //         echo "New record created successfully";
+    //     } else {
+    //         echo "Error: " . $query . "<br>" . $connection->error;
+    //     }
+    //     $connection->close();
+    // }
 ?>
