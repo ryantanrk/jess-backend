2abfe6123c8c15feeda20d9ca05a1d506ff444ea
Ryan <49902829+ryantanrk@users.noreply.github.com>
2021-11-08 14:15:20 +0800

    updated .gitignore

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once 'connection.php';
-    require_once 'class/document.php';
-    header("Access-Control-Allow-Origin: *");
-    header("Content-Type: application/json");
-    
-
-    echo json_encode($document, JSON_PRETTY_PRINT);
-?>
