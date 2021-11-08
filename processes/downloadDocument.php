e2ed058e826ae053d3af8755d4fcf8ea26a56a06
Ryan <49902829+ryantanrk@users.noreply.github.com>
2021-11-08 12:55:24 +0800

    download document works

--- processes/downloadDocument.php
+++ processes/downloadDocument.php
@@ -10,35 +10,41 @@
     $documentID = "";
     $title = "";
     $file = "";
+    $arr = [1 => "send a POST request to this url!"];
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $documentID = $_POST['documentID'];
         //get title & file
         $query = "SELECT `title`, `file` FROM `$documentTable` WHERE documentID = ?";
         $result = sqlProcesses($query, "s", [$documentID]);
 
-        while ($row = mysqli_fetch_assoc($result)) {
-            $title = $row['title'];
-            $file = $row['file'];
+        //if result found
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $title = $row['title'];
+                $file = $row['file'];
+            }
+            //set headers
+            header("Content-Type: application/pdf");
+            header('Content-Disposition: inline; filename="' . $title . '.pdf"');
+            header("Content-Transfer-Encoding: binary");
+            header('Accept-Ranges: bytes');
+
+            //determine filename
+            $filename = $title . ".pdf";
+
+            //create file and write to it
+            $myfile = fopen("../documents/" . $filename, "w") or die("Unable to open file!");
+            fwrite($myfile, $file);
+            fclose($myfile);
+
+            //go to file
+            header('Location: ' . "../documents/" . $filename, true, 302);
         }
-
-        //set headers
-        header("Content-Type: application/pdf");
-        header('Content-Disposition: inline; filename="' . $title . '.pdf"');
-        header("Content-Transfer-Encoding: binary");
-        header('Accept-Ranges: bytes');
-
-        //determine filename
-        $filename = $title . ".pdf";
-
-        //create file and write to it
-        $myfile = fopen("../documents/" . $filename, "w") or die("Unable to open file!");
-        fwrite($myfile, $file);
-        fclose($myfile);
-
-        //go to file
-        header('Location: ' . "../documents/" . $filename, true, 302);
+        else {
+            $arr = ["error" => "document not found"];
+        }
     }
     else {
-        echo json_encode([1 => "send post request"], JSON_PRETTY_PRINT);
+        echo $arr;
     }
 ?>
