bba8123da247fe5176a32ee525714895e751317f
Ryan <49902829+ryantanrk@users.noreply.github.com>
2021-11-13 13:44:05 +0800

    updated config.php

--- config.php
+++ config.php
@@ -6,12 +6,6 @@
 
     //database config
     $database = 'jess'; //database name
-    
-    //table names
-    $documentTable = 'document';
-    $personTable = 'person';
-    $reviewTable = 'review';
-    $reviewerTable = 'reviewerspecific';
 
     $email = "survxyzburner@gmail.com";
 ?>
